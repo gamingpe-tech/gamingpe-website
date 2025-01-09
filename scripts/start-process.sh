dc817c562eb1e4bae691b1b1c5cfa5da1267bbf6
Amanbangashqau <56843708+Amanbangashqau@users.noreply.github.com>
2025-01-09 22:28:06 +0500

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,10 +1,12 @@
-version: 0.0
-os: linux
-files:
-  - source: /
-    destination: /gamingpe-website
-hooks:
-  ApplicationStart:
-    - location: scripts/start-process.sh
-      timeout: 3000
-      runas: root
+#!/bin/bash
+sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website
+sudo docker rm -f $(sudo docker ps -q -a --filter "name=gamingpe-website")
+sudo docker rmi -f $(sudo docker images "376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest" -q)
+sleep 3
+sudo docker pull 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
+if [[ $? != 0 ]]
+then
+    sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 376129854991.dkr.ecr.ap-south-1.amazonaws.com
+    sudo docker pull 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
+fi
+sudo docker run -d -p 8080:8080 --name gamingpe-website 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
