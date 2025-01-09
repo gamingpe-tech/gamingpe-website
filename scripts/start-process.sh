cc35f93b57a5865767e4eba78dd68d9ee1f6de9e
Amanbangashqau <56843708+Amanbangashqau@users.noreply.github.com>
2025-01-09 22:49:45 +0500

    Update start-process.sh

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -9,4 +9,4 @@ then
     sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 376129854991.dkr.ecr.ap-south-1.amazonaws.com
     sudo docker pull 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
 fi
-sudo docker run -d -p 3000:3000 --name gamingpe-website 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
+sudo docker run -d -p 5173:5173 --name gamingpe-website 376129854991.dkr.ecr.ap-south-1.amazonaws.com/gamingpe-website:latest
