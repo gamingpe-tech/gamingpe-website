6af9e31bf56a9db4918606aa317c7bff15cb4956
Aftab Ahmed <aftabahmedkhan464@gmail.com>
2025-01-15 17:41:12 +0400

    homeMap.mp4 name fixed

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { sectionData } from "../../mockData/sectionData";
-import { Link } from "react-router-dom";
-import HomeWMap from "./homeWmap.mp4";
+import HomeWMap from "./homeMap.mp4";
 import Flag from "react-world-flags"; // Import the flag component
 import "aos/dist/aos.css"; // Import AOS styles
 import AOS from "aos";
@@ -22,29 +21,45 @@ const Hero = () => {
     AOS.init({ duration: 1000 }); // Initialize AOS with animation duration
   }, []);
 
-  const { title, title2, title3, countries_image } =
-    sectionData.hero;
+  const { title, title2, title3 } = sectionData.hero;
 
   return (
     <section className="Hero">
       {/* Main Content Layer */}
       <div className="content-container">
-        <h3 className="global-gradient-text" data-aos="fade-right">{title}</h3>
-        <h3 data-aos="fade-right" data-aos-delay="200">{title2}</h3>
-        <h3 data-aos="fade-right" data-aos-delay="400">{title3}</h3>
-        <p className="hero-description" data-aos="fade-right" data-aos-delay="600">
+        <h3 className="global-gradient-text" data-aos="fade-right">
+          {title}
+        </h3>
+        <h3 data-aos="fade-right" data-aos-delay="200">
+          {title2}
+        </h3>
+        <h3 data-aos="fade-right" data-aos-delay="400">
+          {title3}
+        </h3>
+        <p
+          className="hero-description"
+          data-aos="fade-right"
+          data-aos-delay="600"
+        >
           Unlocking New Horizons For International Growth.
         </p>
-        <div className="hero-description2" data-aos="fade-right" data-aos-delay="800">
+        <div
+          className="hero-description2"
+          data-aos="fade-right"
+          data-aos-delay="800"
+        >
           Keep transactions secure with advanced fraud detection.
         </div>
-        <div className="flags-container" style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "5px",
-          marginTop: "20px",
-          justifyContent: "left",
-        }}>
+        <div
+          className="flags-container"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "5px",
+            marginTop: "20px",
+            justifyContent: "left",
+          }}
+        >
           {countries.map((country, index) => (
             <div
               key={index}
